d83cb9da4c44ccdaa2f67998f159a6e9abff7a75
DanielGeek <daniel30081990@gmail.com>
2017-12-24 11:47:26 -0400

    Agregado editar usuario

--- js/user.js
+++ js/user.js
@@ -27,6 +27,7 @@ $(document).ready(function(){
         "pageLength": 25
     });
 
+    // cuando enviamos la data del formulario de usuario en el modal
     $(document).on('submit', '#user_form', function(event){
         event.preventDefault();
         $('#action').attr('disabled','disabled');
@@ -44,5 +45,26 @@ $(document).ready(function(){
           userdataTable.ajax.reload();
          }
         })
+       });
+
+       $(document).on('click', '.update', function(){
+           var user_id = $(this).attr("id");
+           var btn_action = 'fetch_single';
+           $.ajax({
+               url:"user_action.php",
+               method:"POST",
+               data:{user_id:user_id, btn_action:btn_action},
+               dataType:"json",
+               success:function(data){
+                   $('#userModal').modal('show');
+                   $('#user_name').val(data.user_name);
+                   $('#user_email').val(data.user_email);
+                   $('.modal-title').html("<i class='fa fa-pencil-square-o'></i> Edit User");
+                   $('#user_id').val(user_id);
+                   $('#action').val('Edit');
+                   $('#btn_action').val('Edit');
+                   $('#user_password').attr('required', false);
+               }
+           })
        });
 });
--- user_action.php
+++ user_action.php
@@ -27,6 +27,53 @@ if(isset($_POST['btn_action']))
    echo 'New User Added';
   }
  }
+
+ if($_POST['btn_action'] == 'fetch_single'){
+     $query = "
+        select * from user_details where user_id = :user_id
+     ";
+     $statement = $connect->prepare($query);
+     $statement->execute(
+         array(
+             ':user_id' => $_POST["user_id"]
+            )
+        );
+        $result = $statement->fetchAll();
+        foreach($result as $row){
+            $output['user_email'] = $row['user_email'];
+            $output['user_name'] = $row['user_name'];
+        }
+
+        echo json_encode($output);
+    }
+
+    if($_POST['btn_action'] == 'Edit'){
+        if($_POST['user_password'] != ''){
+            $query = "
+                update user_details set
+                user_name = '".$_POST["user_name"]."',
+                user_email = '".$_POST["user_email"]."',
+                user_password = '".password_hash($_POST["user_password"], PASSWORD_DEFAULT)."'
+                where user_id = '".$_POST["user_id"]."'
+            ";
+        } else {
+            
+            $query = "
+                UPDATE user_details SET
+                    user_name = '".$_POST["user_name"]."',
+                    user_email = '".$_POST["user_email"]."'
+                    WHERE user_id = '".$_POST["user_id"]."'
+                ";
+        }
+        $statement = $connect->prepare($query);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        if(isset($result)){
+            
+            echo 'User Details Edite';
+        }
+
+    }
 }
 
 ?>
